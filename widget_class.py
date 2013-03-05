ea66a3186add464a283cddd86bb8a3ef734ca9f4
mattphotonman <matt@photon.man>
2013-03-04 22:10:21 -0500

    Finished __init__ and wrote parse_int function.

--- widget_class.py
+++ widget_class.py
@@ -31,11 +31,35 @@ class Widget:
         re-rolls); 3 is the default value as in the ordinary
         game of Yahtzee.
         """
-        if (type(points)==str):
+        self.n_dice=parse_int(n_dice,"n_dice",1)
+        self.n_faces=parse_int(n_faces,"n_faces",1)
+        self.n_rolls=parse_int(n_rolls,"n_rolls",1)
+        
+        if type(points)==str:
             self.parse_points_str(points)
-        elif (type(points)==dict):
+        elif type(points)==dict:
             self.parse_points_dict(points)
         else:
             print >> sys.stderr, "Error in Widget.__init__: points must be a string or dictionary."
             exit()
         
+    def parse_points_str(self,points):
+        pass
+
+def parse_int(n,name,lower=None,upper==None):
+    """
+    Checks that n is a type int, and that it is in
+    the bounds [lower,upper] if provided.
+    """
+    if type(n)!=int:
+        print >> sys.stderr, "Error:", name, "not type int."
+        exit()
+
+    if lower!=None and n<lower:
+        print >> sys.stderr, "Error:", name, "must be >=", lower
+        exit()
+
+    if upper!=None and n>upper:
+        print >> sys.stderr, "Error:", name, "must be <=", upper
+        exit()
+
